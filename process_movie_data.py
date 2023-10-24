fc7c4bcd35107f0e71675924623d42f46241e698
Hasana Parker <parkerhasana@gmail.com>
2023-10-24 10:39:01 -0700

    changed to top_N with default value being  10

--- process_movie_data.py
+++ process_movie_data.py
@@ -3,12 +3,10 @@
 import csv
 
 
-def find_top_n(filename, n=5):
-    """Finds the top n highest grossing movies in a CSV dataset.
-       Input: filename, a string - points to filename of dataset
-       Output: None
-       Effect: should print n lines of text
-    """
+
+def find_top_n(filename, n=10):
+    """Finds the top n highest grossing movies in a CSV dataset."""
+
     # read in file contents as list of dictionaries
     with open(filename) as f:
         csvr = csv.DictReader(f)
@@ -22,10 +20,11 @@ def find_top_n(filename, n=5):
     # Sort data and get top 10
     gross_sort = lambda x : x["Gross"]
     rows.sort(key=gross_sort)
-    top_ten = rows[:-n+1:-1]
+
+    top_n = rows[:-n-1:-1]
 
     # Print out results
-    for i, row in enumerate(top_ten):
+    for i, row in enumerate(top_n):
         print("{ind}. {row[Title]} ({row[Year]}) - ${row[Gross]:,d}".format(
             ind=i+1,
             row=row))
@@ -35,4 +34,5 @@ def find_top_n(filename, n=5):
 # Movie data comes from "Movie Gross and Ratings" dataset on Kaggle by Yashwanth Sharaf
 # https://www.kaggle.com/datasets/thedevastator/movie-gross-and-ratings-from-1989-to-2014
 if __name__ == "__main__":
+
     find_top_n("Movies_gross_rating.csv")
